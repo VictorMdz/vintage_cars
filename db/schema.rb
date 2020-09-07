82c72b51596fe0bda3d4bd521da23b4b8131ab19
Mara Chimeno <marachimeno@gmail.com>
2020-09-07 18:26:19 +0200

    added end to schema

--- db/schema.rb
+++ db/schema.rb
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2020_09_07_154049) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
 
   create_table "cars", force: :cascade do |t|
     t.string "brand"
