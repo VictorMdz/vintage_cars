d1efbd2fccf58efbebc351438d248bdd059c3f52
Mara Chimeno <marachimeno@gmail.com>
2020-09-08 17:42:32 +0200

    added pundit authorize methods to bookings controller

---
+++ app/policies/booking_policy.rb
@@ -0,0 +1,20 @@
+class BookingPolicy < ApplicationPolicy
+
+  def show?
+    true
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+end
