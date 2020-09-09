b86e29af1f79b9334b02a713a5a879c1042dcef4
Camille Supery <csupery@gmail.com>
2020-09-09 14:42:23 +0200

    Added validations in Car and User models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :cars
-  has_many :bookings
+  has_many :cars, dependent: :destroy
+  has_many :bookings, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
